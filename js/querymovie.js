9309f85ec791096abddd44203403d8475126fe0a
Ching-Yi Lin <icequ60914@gmail.com>
2017-12-07 04:14:36 -0800

    adding interaction, including categories, search, filter, and sort

--- js/querymovie.js
+++ js/querymovie.js
@@ -1,7 +1,14 @@
+var currCat = "cat=0";
+var recId = -1;
+var searchStr = null;
+var filterGenre = null;
+var filterTime = null;
+var mode = "movie"; //search, rec, movie
 function getMovieData(query) {
-    var baseURI = "http://localhost:8081/";
+    var baseURI = "http://localhost:8082/";
     url = baseURI + query;
-    return new Prmoise((resolove, reject) => {
+    //console.log(url);
+    return new Promise((resolove, reject) => {
         xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = () => resolove(xhr.responseText);
@@ -10,31 +17,110 @@ function getMovieData(query) {
     });
 }
 
+function getMovieDataOld(query, done) {
+    var baseURI = "http://localhost:8082/";
+    url = baseURI + query;
+    //console.log(url);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function () {
+      done(null, xhr.response);
+    };
+    xhr.onerror = function () {
+      done(xhr.response);
+    };
+    xhr.send();
+}
+
+/*
 function getSimilar(id) {
-    getMovieData(id).then(function(response) {
-        updatResult(response);
+    var query = "rec/" + id;
+    console.log(query);
+    getMovieDataOld(query).then(function(response) {
+        updateResult(response);
     }, function(Error) {
         console.log(Error);
     });
+}*/
+
+function search(userInput) {
+    userInput = userInput.replace(/ /g,"+");
+    searchStr = userInput;
+    mode = "search";
+    var query = "search/" + userInput;
+    //console.log(query);
+    getMovieDataOld(query, function(err, data) {
+        if(err) {throw err;}
+        console.log(data);
+        resetFilter();
+        updateResult(data);
+  })};
+
+function getCategory(input) {
+    var query = "movie?"+input;
+    mode = "movie";
+    console.log(query);
+    getMovieDataOld(query, function(err, data) {
+        if(err) {throw err;}
+        console.log(data);
+        resetFilter();
+        updateResult(data);
+  })};
+
+  function getSimilar(id) {
+    recId = id;
+    mode = "rec";
+    var query = "rec/" + id;
+    //console.log(query);
+    getMovieDataOld(query, function(err, data) {
+        if(err) {throw err;}
+        console.log(data);
+        resetFilter();
+        updateResult(data);
+  })};
+
+function refreshMovie(query) {
+    getMovieDataOld(query, function(err, data) {
+        if(err) {throw err;}
+        console.log(data);
+        resetFilter();
+        updateResult(data);
+  });
+}
+
+function resetFilter() {
+    filterGenre = null;
+    filterTime = null;
+}
+
+function resetMode() {
+    mode = "movie";
 }
 
 function updateResult(data) {
+    $("#result").empty();
+    var movies = JSON.parse(data).movies;
+    if (movies == "No result" || movies == "") {
+        $("#result").empty();
+        $("#result").append('<h5 class="white-text"> Oops, sorry, there is no search result for you : ( </h5>');
+        return;
+    }
     var movies = JSON.parse(data).movies;
     var rowId;
-    $("#result").empty();
+    
     for (i in movies){
         if(i % 4 == 0) {
             rowId = "row" + i/4;
             $("#result").append('<div class ="row" id="'+ rowId + '">');
         }
-        $("#"+rowId).append('<div class="col s12 m4 l3" id ="' + movies[i]['id'] + '">\
-            <div class="card">\
+        $("#"+rowId).append('<div class="col s6 m3" id ="' + movies[i]['id'] + '">\
+            <div class="card hoverable">\
             <div class="card-image waves-effect waves-block waves-light">\
                 <img class="activator" src="http://image.tmdb.org/t/p/w342' + movies[i]['poster_path'] +'">\
             </div>\
             <div class="card-content">\
-                <span class="card-title activator grey-text text-darken-4">' + movies[i]['original_title'] + '<i class="material-icons right">more_vert</i></span>\
-                <button class="btn waves-effect waves-light" onclick="getMovieData(' + movies[i]['id'] + ')">Get Similar Movie</button>\
+                <span class="one-line card-title activator grey-text text-darken-4">' + movies[i]['original_title'] + '<i class="material-icons right"></i></span>\
+                <button class="btn waves-effect waves-light" onclick="getSimilar(' + movies[i]['id'] + ')">Get Similar Movie</button>\
             </div>\
             <div class="card-reveal">\
                 <span class="card-title grey-text text-darken-4">' + movies[i]['original_title'] + '<i class="material-icons right">close</i></span>\
@@ -45,13 +131,201 @@ function updateResult(data) {
         ;
     }
     //var keyword = response['Predictions'][0]['Tag'];
-    
+    $('.one-line').ellipsis({lines: 1});
 }
 
-function hello(){
-    alert("hello");
-}
 
 var myJSON = '{"movies":[{"id": "123","poster_path": "/vzmL6fP7aPKNKPRTFnZmiUfciyV.jpg","overview": "abcdefg","original_title": "movie1","language": "English", "release_date": "10/30/95", "genre_list": ["Comedy", "Drama", "Romance"],"popularity": 21.946,"vote_average": 7.7},{"id": "145","poster_path": "/vzmL6fP7aPKNKPRTFnZmiUfciyV.jpg","overview": "abcdefg","original_title": "movie2","language": "English", "release_date": "10/30/97", "genre_list": ["Animation", "Comedy", "Family"],"popularity": 21.946,"vote_average": 7.7}]}';
 //var myObj = JSON.parse(myJSON);
 //document.getElementById("demo").innerHTML = myObj.movies[0]['id'];
+
+$(".filter1").click(function(e){
+    var query;
+    console.log(e.target.id);
+    switch(mode) {
+        case 'movie':
+            query = "movie?" + currCat + "&filter1=";
+            break;
+        case 'search':
+            query = "search/" + searchStr + "&filter1=";
+            break;
+        case 'rec':
+            query = "rec/" + recId + "&filter1=";
+            break;
+        default:
+            console.log("mode error");
+    }
+    filterTime = e.target.id;
+    switch(filterTime) {
+        case 'ft1':
+            filterTime = 1;
+            break;
+        case 'ft2':
+            filterTime = 2;
+            break;
+        case 'ft3':
+            filterTime = 3;
+            break;
+        default:
+            console.log("filter error");
+    }
+    query = query + filterTime;
+    if(filterGenre != null)
+        query = query + "&filter2=" + filterGenre;
+    refreshMovie(query);
+})
+
+$(".filter2").click(function(e){
+    var query;
+    console.log(e.target.id);
+    filterGenre = e.target.id;
+    switch(mode) {
+        case 'movie':
+            query = "movie?" + currCat + "&filter2=";
+            break;
+        case 'search':
+            query = "search/" + searchStr + "&filter2=";
+            break;
+        case 'rec':
+            query = "rec/" + recId + "&filter2=";
+            break;
+        default:
+            console.log("mode error");
+    }
+    query = query + filterGenre;
+    if(filterTime != null)
+        query = query + "&filter1=" + filterTime;
+    refreshMovie(query);
+})
+
+$("#searchbar").submit(function (e){
+    console.log($("#search").val());
+    e.preventDefault();
+    search($("#search").val());
+  });
+
+$("input").click(function (e) {
+    var sortby;
+    var query;
+    switch(mode) {
+        case 'movie':
+            query = "movie?" + currCat;
+            break;
+        case 'search':
+            query = "search/" + searchStr;
+            break;
+        case 'rec':
+            query = "rec/" + recId;
+            break;
+        default:
+            console.log("mode error");
+    }
+
+    switch($("#sort input:radio:checked").val()){
+        case '0':
+            sortby = "sort=release_date&ascending=0";
+            break;
+        case '1':
+            sortby = "sort=release_date&ascending=1";
+            break;
+        case '2':
+            sortby = "sort=popularity&ascending=0";
+            break;
+        case '3':
+            sortby = "sort=popularity&ascending=1";
+            break;
+        case '4':
+            sortby = "sort=vote_average&ascending=0";
+            break;
+        case '5':
+            sortby = "sort=vote_average&ascending=1";
+            break;
+        default:
+        sortby = "sort=release_date&ascending=0";
+    }
+    
+    query = query + '&'+sortby;
+    if (filterGenre != null)
+        query = query + "&filter2=" + filterGenre;
+    if (filterTime != null)
+        query = query + "&filter1=" + filterTime;
+    refreshMovie(query);
+});
+
+$('#start').click(function (e){
+    console.log('start');
+    $('#landing').hide();
+    $('#category').show();
+    resetFilter();
+    
+  });
+  
+$('.cat').click(function (e) {
+    currCat = e.target.id;
+    console.log(currCat);
+    var query = "movie?" + currCat;
+    getMovieDataOld(query, function(err, data) {
+        if(err) {throw err;}
+        //console.log(data);
+        updateResult(data);
+    }) 
+});
+
+// $('#cat1').click( ()=> 
+//     getMovieDataOld("movie?cat=1", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "1";
+//     updateResult(data); 
+//   }));
+ 
+// $('#cat2').click( ()=> 
+//   getMovieDataOld("movie?cat=2", function(err, data) {
+//   if(err) {throw err;}
+//   //console.log(data);
+//   currCat = "2";
+//   updateResult(data); 
+// }));
+// $('#cat3').click( ()=> 
+//     getMovieDataOld("movie?cat=3", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "3";
+//     updateResult(data); 
+// }));
+// $('#cat4-1').click( ()=> getMovieDataOld("movie?cat=4-1", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "4-1";
+//     updateResult(data); 
+// }));
+// $('#cat4-2').click( ()=> getMovieDataOld("movie?cat=4-2", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "4-2";
+//     updateResult(data); 
+// }));
+// $('#cat5').click( ()=> getMovieDataOld("movie?cat=5", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "5";
+//     updateResult(data); 
+// }));
+// $('#cat6').click( ()=> getMovieDataOld("movie?cat=6", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "6";
+//     updateResult(data); 
+// }));
+// $('#cat7').click( ()=> getMovieDataOld("movie?cat=7", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "7";
+//     updateResult(data); 
+// }));
+// $('#cat8').click( ()=> getMovieDataOld("movie?cat=8", function(err, data) {
+//     if(err) {throw err;}
+//     //console.log(data);
+//     currCat = "8";
+//     updateResult(data); 
+// }));
